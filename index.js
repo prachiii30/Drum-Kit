48b0c6be0a889f84f3f4130a51c803cb8f81d148
Prachi Parihar <prachisingh30aabb@gmail.com>
2023-06-20 10:40:38 +0530

    Modified File Added

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++){ 
 document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-    alert("I have been clicked")
+   this.style.color="white";
+   var innerbutton=this.innerHTML;
+   
+   switch(innerbutton){
+    case "w":
+        var audio= new Audio('/sounds/tom-1.mp3');
+         audio.play();
+         break;
+    case "a":
+        var audio= new Audio('/sounds/tom-2.mp3');
+         audio.play();
+         break;
+    case "s":
+        var audio= new Audio('/sounds/tom-3.mp3');
+         audio.play();
+         break;
+    case "d":
+        var audio= new Audio('/sounds/tom-4.mp3');
+        audio.play();
+        break;
+    case "j":
+        var audio= new Audio('/sounds/snare.mp3');
+        audio.play();
+        break;
+    case "k":
+        var audio= new Audio('/sounds/kick-bass.mp3');
+         audio.play();
+         break;
+    case "l":
+        var audio= new Audio('/sounds/crash.mp3');
+         audio.play();
+         break;
+    default:
+        alert("Press From the given choice");
+        
+         
+    }
 });
-
 }
+
+
+
+
+
+
 
